00c3c241c2c5955767d54a15ce550a051004533a
Eric Edward Finer <eric.e.finer@gmail.com>
2017-05-05 13:57:11 -0400

    add indexes to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 User.create!([
-  {username: "h4nkGr1ll", email: "hank@propane.edu", password_digest: "$2a$10$WT35IlfJMDgmAfOe1HirIepr5Bkj7Z6NbAOXuf3j9mErYnQhlEfx.", session_token: "P_3D_Ho71QAdgbctXlMTqw", bio: "the guest account for demos", image_url: nil, photo_file_name: nil, photo_content_type: nil, photo_file_size: nil, photo_updated_at: nil},
-  {username: "3DgUyF", email: "guyf@tripleD.gov", password_digest: "$2a$10$rty0kroGKQqG2gl5JQm1/egftr3YbzXZ3cW58fxO9j8L2fmeCqjte", session_token: "0Ibf93OV_Bc2GxE8nBQWFQ", bio: nil, image_url: nil, photo_file_name: "avatar.jpg", photo_content_type: "image/jpeg", photo_file_size: 137105, photo_updated_at: "2017-03-13 20:12:23"}
+  {username: "h4nkGr1ll", email: "hank@propane.edu", password_digest: "$2a$10$WT35IlfJMDgmAfOe1HirIepr5Bkj7Z6NbAOXuf3j9mErYnQhlEfx."},
+  {username: "3DgUyF", email: "guyf@tripleD.gov", password_digest: "$2a$10$rty0kroGKQqG2gl5JQm1/egftr3YbzXZ3cW58fxO9j8L2fmeCqjte"}
 ])
