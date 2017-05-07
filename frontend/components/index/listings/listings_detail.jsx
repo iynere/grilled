a7c622c7377e141d93597a1d8c56945128062a52
Eric Edward Finer <eric.e.finer@gmail.com>
2017-05-07 14:58:45 -0400

    add basic styles and build out ListingDetail component

--- frontend/components/index/listings/listings_detail.jsx
+++ frontend/components/index/listings/listings_detail.jsx
@@ -13,14 +13,39 @@ class ListingDetail extends React.Component {
     if (this.props.listing === null) return (<div className="loading">loading</div>);
     return (
       <div>
-        <section>
+        <section className="listingInfo">
           <h1>{this.props.listing.name}</h1>
           <h3>{this.props.listing.brand}</h3>
           <h3>{this.props.listing.price}</h3>
+          <p>
+            {this.props.listing.description}
+          </p>
         </section>
+        <section className="listingButtons">
+          <button className="btn btn-square">buy now</button>
+          <button className="btn btn-square">make offer</button>
+          <button className="btn btn-square">message seller</button>
+        </section>
+        <span>Sold by {this.props.listing.username}</span>
       </div>
     );
   }
 }
+
+ListingDetail.propTypes = {
+  listing: PropTypes.shape({
+    name: PropTypes.string,
+    brand: PropTypes.string,
+    price: PropTypes.number,
+    description: PropTypes.string,
+    username: PropTypes.string,
+  }),
+  fetchListing: PropTypes.func.isRequired,
+  clearListing: PropTypes.func.isRequired,
+};
+
+ListingDetail.defaultProps = {
+  listing: null,
+};
 
 export default ListingDetail;
