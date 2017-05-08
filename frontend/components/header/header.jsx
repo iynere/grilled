c209d4471c626235f23326afe49bb5dc99d2fe12
Eric Edward Finer <eric.e.finer@gmail.com>
2017-05-08 16:05:08 -0400

    add link to messages path

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = ({ currentUser, logout }) => {
   const UserActions = () => (
     <nav>
       <span>{currentUser.username}</span>
+      <Link className="btn btn-square red" to={'/messages'}>Messages</Link>
       <button className="btn btn-square blue" onClick={logout}>Sign Out</button>
     </nav>
   );
   return (
     <header>
       <section className="logo">
-        <h1 >GRILLED</h1>
+        <Link to={'/'}>
+          <h1>GRILLED</h1>
+        </Link>
       </section>
       <section>
         { currentUser === null ? "High-End BBQ Marketplace" : UserActions() }
