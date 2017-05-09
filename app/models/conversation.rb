c8289f8959d70da2bc256e1b0a6f72f41386ffed
Eric Edward Finer <eric.e.finer@gmail.com>
2017-05-09 15:52:01 -0400

    remove unnecessary class methods for Conversation and associated specs

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -6,24 +6,4 @@ class Conversation < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :recipient, class_name: "User"
   belongs_to :listing
-
-  def self.create_from_message(options)
-    conversation = Conversation.create(options)
-
-    if conversation.save
-      return conversation.id
-    end
-
-    false
-  end
-
-  def self.update_offer(offer, id)
-    conversation = Conversation.find(id)
-
-    if conversation.update({offer: offer})
-      return offer
-    end
-
-    false
-  end
 end
