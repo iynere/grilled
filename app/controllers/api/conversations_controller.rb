54909168807336ac8842947f66ff9b64c71aec5b
Eliot Asenoth Gaspar-Finer <eric.e.finer@gmail.com>
2017-05-11 12:09:56 -0400

    add require_logged_in before action callback to relevant controllers

--- app/controllers/api/conversations_controller.rb
+++ app/controllers/api/conversations_controller.rb
@@ -1,4 +1,6 @@
 class Api::ConversationsController < ApplicationController
+  before_action :require_logged_in
+  
   def index
     @conversations = Conversation.where(recipient: current_user)
       .or(Conversation.where(sender: current_user))
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,6 @@
 class Api::MessagesController < ApplicationController
+  before_action :require_logged_in
+  
   def create
     @message = Message.new(message_params)
 
